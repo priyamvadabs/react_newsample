68a9b22c17fabac96b40884758bb6d9f31e9aa59
priyamvadabs <priyamvada@kompetenzen.in>
2026-05-23 10:32:31 +0530

    home page has been modified

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+        <h1>Home Page</h1>
+        <Link to="/about">
+        <button>Go To About Page</button>
+        </Link>
+
+    </div>
   )
 }
 
