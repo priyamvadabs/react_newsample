14b5c08eb2e0f7771cdd1f7b0051c81c61e888c1
sabarinath20032003-debug <sabarinath20032003@gmail.com>
2026-05-23 10:37:04 +0530

    changed service

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Service() {
   return (
-    <div>Service</div>
+    <div>
+        <h1>Service Page</h1>
+        <Link to="/home">
+        <button>Go To Home Page</button>
+        </Link>
+
+    </div>
   )
 }
 
